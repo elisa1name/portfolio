162d53fcdedbf05ce4067a7bf1d2543c418adacd
Elisa Vallee <elisavallee@MacBook-Pro-de-Elisa-2.local>
2019-04-30 14:53:23 +0200

    deuxieme push

--- script.js
+++ script.js
@@ -1,21 +1,102 @@
-$(document).ready(function()
-{
-	navbar_onscroll();
+var modal = document.getElementById('myModal');
 
-	function navbar_onscroll()
-	{
-		if ($(this).scrollTop() >= 1)
-		{
-			$('.navbar').addClass('active'); 
-		}
-		else
-		{
-			$('nav').removeClass('active'); 
-		}
-	}
 
-	$(window).scroll(function()
-	{	
-		navbar_onscroll();
-	});
-});
+var img = document.getElementById('myImg');
+var img1 = document.getElementById('myImg1');
+var img2 = document.getElementById('myImg2');
+var img3 = document.getElementById('myImg3');
+var img4 = document.getElementById('myImg4');
+var img5 = document.getElementById('myImg5');
+var img6 = document.getElementById('myImg6');
+var img7 = document.getElementById('myImg7');
+var img8 = document.getElementById('myImg8');
+var img9 = document.getElementById('myImg9');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  console.log(modal);
+  
+  captionText.innerHTML = this.alt;
+}
+
+img1.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img3.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img4.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img5.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img6.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img7.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img8.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+img9.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    console.log(modal);
+    
+    captionText.innerHTML = this.alt;
+}
+
+
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
